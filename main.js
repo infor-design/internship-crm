65d7d35c892f6788c5fbe6b45e57f204eb045bca
Griffin Blotner <gblotner@USNYMGBLOTNER11.local>
2020-06-15 12:56:43 -0400

    I modified the main.js to fix the root directories

--- main.js
+++ main.js
@@ -6,11 +6,13 @@ var app = express();
 var path = require('path');
 var port = 8000;
 
-app.use('/css', express.static(__dirname + '/css'));
-app.use('/img', express.static(__dirname + '/img'));
+app.use('/public/css', express.static(__dirname + '/public/css'));
+app.use('/public/img', express.static(__dirname + '/public/img'));
+app.use('/public/html', express.static(__dirname + '/public/html'));
+app.use('/public',express.static(__dirname + '/public'));
 
-app.get('/', function(req,res){
-    res.sendFile(path.join(__dirname + '/customer-list.html'));
+app.get('/public/html', function(req,res){
+    res.sendFile(path.join(__dirname + '/public/html/customer-list.html'));
 }
 
 
